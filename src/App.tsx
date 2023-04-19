274fd8e2bffac46837e301aadf62ac418a6a768c
Alex Sandro Martins de Araújo <131191686+AlexSMAraujo@users.noreply.github.com>
2023-04-19 08:56:56 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const App = () => {
+
+    return (
+        <SafeAreaView>
+            <Text> TESTANDO</Text>
+        </SafeAreaView>
+    )
+};
+
+export default App;
